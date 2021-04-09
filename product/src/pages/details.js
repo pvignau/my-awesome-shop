2c9a410b090538021e1e143af732fb24ebd4d40e
Zarema Khalilova <z.halilova@3commas.io>
2021-04-09 13:44:12 +0300

    Use ProductCardDetails on details page

--- product/src/pages/details.js
+++ product/src/pages/details.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { Button, Icon } from "design-system";
+import { Button, Icon, ProductCardDetails } from "design-system";
 import itemImg from "../assets/item1.png";
 
 const StyledDiv1 = styled.div`
@@ -23,36 +23,6 @@ const StyledDiv4 = styled.div`
   display: flex;
   flex-direction: column;
 `;
-const StyledDiv5 = styled.div`
-  border-radius: 32px;
-  margin-bottom: 24px;
-  position: relative;
-`;
-
-const StyledButton = styled(Button)`
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: 1;
-  margin: 24px;
-`;
-
-const StyledImg = styled.img`
-  max-width: 100%;
-  display: block;
-`;
-
-const StyledP = styled.p`
-  margin-bottom: 8px;
-  opacity: 0.6;
-`;
-const Styledh1 = styled.h1`
-  margin-bottom: 16px;
-`;
-const Styledh2 = styled.h2`
-  color: #f15223;
-  margin-bottom: 40px;
-`;
 const Details = () => {
   return (
     <div>
@@ -66,17 +36,7 @@ const Details = () => {
           </StyledDiv3>
         </StyledDiv2>
         <StyledDiv4>
-          <StyledDiv5>
-            <StyledButton isOutline>
-              <Icon name="heart" />
-            </StyledButton>
-            <StyledImg src={itemImg} alt="" />
-          </StyledDiv5>
-          <StyledP> Men's Shoe</StyledP>
-          <Styledh1>
-            Nike Air Force 1{"\u00A0"}'07{"\u00A0"}LX
-          </Styledh1>
-          <Styledh2>€95.00</Styledh2>
+          <ProductCardDetails imageSrc={itemImg} name={`Nike Air Force 1\u00A0'07\u00A0LX`} price="€95.00" category=" Men's Shoe" />
           <Button>Add to Cart</Button>
         </StyledDiv4>
       </StyledDiv1>
